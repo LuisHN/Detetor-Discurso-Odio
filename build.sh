b206f17c11c38a4eb7448995ad422743214f3a9c
Luis Henrique <luishenriqueneves1992@gmail.com>
2022-09-24 12:10:17 +0100

    Update build

--- build.sh
+++ build.sh
@@ -15,4 +15,9 @@ npm run build
 cd /opt/work/Detetor-Discurso-Odio/classificador/worker
 pip install -r requirements.txt
 
-pm2 restart all
+pm2 stop all
+pm2 start classificador.py --name classificador --interpreter python3
+
+cd  /opt/work/Detetor-Discurso-Odio/backend/dist
+pm2 start dist/main.js --name backend
+
